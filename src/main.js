d79db564240044885da54821da1eaace436e360b
Evelina <evelina.nuca@girlsgoit.org>
2019-07-27 22:55:39 +0300

    Header component

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import VueRouter from 'vue-router';
-import Home from './components/Home.vue'
+import Home from './components/Home.vue';
+import Settings from './components/Settings.vue';
+import Help from './components/Help.vue';
+import SignIn from './components/SignIn.vue'
 
 Vue.use(VueRouter);
 
-const rute = [{
-  path: "/",
-  component: Home
-}, ];
+const rute = [
+  {path: "/home",component: Home}, 
+  {path: '/settings',component: Settings},
+  {path:'/help', component: Help} ,
+  {path:'/sign-in', component: SignIn}
+
+];
 
 const router = new VueRouter({
   routes: rute,
