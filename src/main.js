88b3ec6d09749475b832ce1fb2765f0b2a52c36c
Marc Graciov <mark.graciov@gmail.com>
2019-07-27 11:58:54 +0300

    set base url for Axios

--- src/main.js
+++ src/main.js
@@ -2,23 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue'
+import Axios from 'axios'
 
 Vue.use(VueRouter);
 
 const rute = [{
-  path: "/",
-  component: Home
-}, ];
+    path: "/",
+    component: Home
+},];
 
 const router = new VueRouter({
-  routes: rute,
-  mode: 'history'
+    routes: rute,
+    mode: 'history'
 });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 
 new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
+    render: h => h(App),
+    router
+}).$mount('#app');
+
+Axios.defaults.baseURL = 'https://notes-api.girlsgoit.org/';
+
