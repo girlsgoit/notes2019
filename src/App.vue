7721cd7d7f98b3ec210a09e9f0a171a3582880a5
Marc Graciov <mark.graciov@gmail.com>
2019-07-27 15:45:58 +0300

    disable console log rule

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <Footer />
+    <Footer/>
   </div>
 </template>
 
 <script>
-import Footer from "./components/Footer.vue";
+    import Footer from "./components/Footer.vue";
 
-export default {
-  name: "app",
-  components: {
-    Footer
-  }
-};
+    export default {
+
+        name: "app",
+        components: {
+            Footer
+        }
+    };
 </script>
 
 <style>
