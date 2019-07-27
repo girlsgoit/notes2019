bc6695bba9064a0e6c24a21d924f6d747124744d
Crick <seong92@mail.ru>
2019-07-27 04:30:58 -0700

    NoteDetails

--- src/main.js
+++ src/main.js
@@ -1,28 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
+import Note from './components/Note.vue'
 import Home from './components/Home.vue'
 import Axios from 'axios'
 
-Vue.use(VueRouter);
+Axios.defaults.baseURL = 'https://notes-api.girlsgoit.org/';
 
-const rute = [{
-    path: "/",
-    component: Home
-},];
+Vue.use(VueRouter);
+const rute = [
+  {
+  path: "/", component: Home
+  }, 
+  { 
+    path: "/note/:id", component: Note 
+  },
+];
 
 const router = new VueRouter({
-    routes: rute,
-    mode: 'history'
+  routes: rute,
+  mode: 'history'
 });
 
 Vue.config.productionTip = false;
 
-
 new Vue({
-    render: h => h(App),
-    router
+  render: h => h(App),
+  router
 }).$mount('#app');
-
-Axios.defaults.baseURL = 'https://notes-api.girlsgoit.org/';
 
