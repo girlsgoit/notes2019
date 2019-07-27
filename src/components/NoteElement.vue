ee938d41678c4d7715109a56bd6124bea7417494
Christy Waickem <waickem@gmail.com>
2019-07-28 01:33:21 +0300

    Dashboard version 3.0

---
+++ src/components/NoteElement.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 v-if="tag == 'h1'">{{ value }}</h1>
+        <h2 v-if="tag == 'h2'">{{ value }}</h2>
+        <h3 v-if="tag == 'h3'">{{ value }}</h3>
+        <h4 v-if="tag == 'h4'">{{ value }}</h4>
+        <a v-if="tag == 'a'" :href="content">Link spre nu stiu unde</a>
+        <img v-if="tag == 'img'" :src="value" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NoteElement',
+    props: ['tag', 'value'],
+}
+</script>
+
+<style scoped>
+
+</style>
