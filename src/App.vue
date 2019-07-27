183652072883d4280c0952e9bfdc4dab7ef9e825
maricika-jz <lacrimioara1maricika@gmail.com>
2019-07-27 16:04:14 +0300

    Home page (components: About, Features, Gallery, Header, Hero, Home)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <Footer />
   </div>
 </template>
 
 <script>
-import Footer from "./components/Footer.vue";
+
 
 export default {
   name: "app",
   components: {
-    Footer
   }
 };
 </script>
